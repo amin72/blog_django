7a05788c65502ced5d2e5ff9cdb0a15fe8dadca6
Amin <asaa47721@gmail.com>
2020-02-24 21:30:44 +0330

    ranking the search results

--- blog/views.py
+++ blog/views.py
@@ -4,7 +4,7 @@ from django.views.generic import ListView
 from django.core.mail import send_mail
 from django.contrib import messages
 from django.db.models import Count
-from django.contrib.postgres.search import SearchVector
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from taggit.models import Tag
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm, SearchForm
@@ -110,8 +110,14 @@ def post_search(request):
         form = SearchForm(request.GET)
         if form.is_valid():
             query = form.cleaned_data['query']
+
+            search_vector = SearchVector('title', 'body')
+            search_query = SearchQuery(query)
+
             results = Post.objects.annotate(
-                search=SearchVector('title', 'body')
-            ).filter(search=query)
+                search=search_query,
+                rank=SearchRank(search_vector, search_query)
+            ).filter(search=search_query).order_by('-rank')
+
     context = {'form': form, 'query': query, 'results': results}
     return render(request, 'blog/post/search.html', context)
